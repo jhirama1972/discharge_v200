032b0c8b6dad65f953962d9c57af902f23f9e5df
KH <j_hirama2@yahoo.co.jp>
2025-06-02 21:06:59 +0900

    commnet add

--- eepdata.h
+++ eepdata.h
@@ -1,9 +1,9 @@
 typedef struct EEP_Data {
-  float calc_v;
+  float calc_v;   // ADD commnet
   float dischg_v;
   float f_normal_target;
   float target_race_current;
   float target_normal_current;
-  boolean hold_mode;
+  boolean hold_mode;  // TEST
   char  password[8]; /* daiki1111*/
 } EEPData, *pEEPData;
